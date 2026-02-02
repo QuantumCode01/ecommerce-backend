779326ffd9411b9751dbb896f7280270b054e26e
QuantumCode01 <121271945+QuantumCode01@users.noreply.github.com>
2026-02-03 01:50:48 +0530

    fix: use smtp.gmail.com instead of gmail service for nodemailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,19 +2,24 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: "smtp.gmail.com",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
     }
   });
 
+  try{
   await transporter.sendMail({
     from: `"EcomCart Support" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html
   });
+  }catch (err) {
+  console.error("Email error:", err);
+  throw new Error("Email failed");
+}
 };
 
 export default sendEmail;
